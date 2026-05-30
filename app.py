63286865cd076a88bdaf08445425b36881b8d194
msadiqsn <51995732+msadiqsn@users.noreply.github.com>
2026-05-30 15:59:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -358,43 +358,6 @@ if st.button("💾 Save"):
     save_to_db(month, fixed_total, variable_total, investment_total, grand_total, var_data)
     st.success("Saved")
 
-# === LOAD DAILY EXPENSES ===
-# === FILTER BY SELECTED MONTH ===
-expense_data = supabase.table("expense_log").select("*").execute().data
-
-expense_df = pd.DataFrame(expense_data)
-
-if not expense_df.empty:
-
-    selected_month_num = month_map[selected_month]
-
-    filter_prefix = f"{selected_year}-{selected_month_num}"
-
-    expense_df = expense_df[
-        expense_df["expense_date"].astype(str).str.startswith(filter_prefix)
-    ]
-
-
-# === LOAD DAILY EXPENSES ===
-# === ALWAYS CREATE DATAFRAME ===
-expense_data = supabase.table("expense_log").select("*").execute().data
-
-expense_df = pd.DataFrame(expense_data)
-
-if expense_df.empty:
-    expense_df = pd.DataFrame(
-        columns=["expense_date", "category", "amount"]
-    )
-else:
-    selected_month_num = month_map[selected_month]
-
-    filter_prefix = f"{selected_year}-{selected_month_num}"
-
-    expense_df = expense_df[
-        expense_df["expense_date"]
-        .astype(str)
-        .str.startswith(filter_prefix)
-    ]
 
 
 
